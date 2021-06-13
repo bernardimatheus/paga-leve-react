c102f450d0c8a73005a6e576027647b33b407a40
Matheus Bernardi <bernardi@pop-os.localdomain>
2021-06-13 04:01:17 -0300

    testing URL change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL: `https://v3b2slwkhg.execute-api.us-east-1.amazonaws.com/dev`,
 });
 
 export default api;
